bb52cbe7e06a764e3a2c6488523d559c68ae1f9c
Tom Marien <tommarien@gmail.com>
2016-02-24 09:42:46 +0100

    Added routing

--- app/app.js
+++ app/app.js
@@ -5,9 +5,24 @@
         .module('myApp', [
             'ui.bootstrap',
             'toaster',
+            'ngRoute',
             'myApp.controllers',
             'myApp.services'
         ])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when('/view1', {
+                    templateUrl: 'views/main.html',
+                    controller: 'UserListController as vm'
+                })
+                .when('/view2', {
+                    templateUrl: 'views/view2.html'
+                })
+                .otherwise({
+                    redirectTo: '/view1'
+                });
+
+        })
         .config(function ($httpProvider) {
             $httpProvider.interceptors.push('httpInterceptor');
         })
